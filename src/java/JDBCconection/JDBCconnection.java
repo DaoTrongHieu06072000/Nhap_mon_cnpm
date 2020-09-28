e16df7ab60f12420da6a43bd9187b03c0ce35261
SonTH183823 <sonto2k@gmail.com>
2020-09-28 18:08:09 +0700

    to hoai son

---
+++ src/java/JDBCconection/JDBCconnection.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDBCconection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sonso
+ */
+public class JDBCconnection {
+    public static Connection getConnection() throws SQLException {
+        final  String url = "jdbc:mysql://localhost:3306/cnpm10";
+        final String user = "root";
+        final String pass = "";      
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //Connection connection = DriverManager.getConnection(url, user, pass);
+            
+            return DriverManager.getConnection(url,user,pass);
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            
+        }
+        return null;
+        
+        
+        
+     }
+     public static void main(String[] args) throws SQLException {
+        Connection c = getConnection();
+        if (c != null) {
+            System.out.println("Thanh cong");
+        }else{
+            System.out.println("ThatBai");
+        }
+        
+     }
+}
